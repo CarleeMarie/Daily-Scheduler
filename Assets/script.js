5ddfcb498b7f54327b65fc7b169882161971b265
Carlee Miller <carlee.miller@gmail.com>
2022-11-22 20:03:09 -0600

    starting the jQuery and day.js coding - done with a tutor

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,10 @@
+console.log("hello world");
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,8 +12,17 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
+  // TODO: 1) Add code to apply the past, present, or future class to each time
+  dayjs().hour(); // gets current hour
+    console.log(dayjs().hour());
+  var timeBlocks = $('.time-block');
+    for (var i = 0; i < timeBlocks.length; i++) {
+     var timeBlockHour = $(timeBlocks[i]).attr('id');
+     console.log(timeBlockHour);
+    }
+
+    
+  //block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
@@ -19,5 +31,9 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  // Adds code to display the current date in the header of the page.
+  var currentDate = dayjs().format('dddd, MMMM DD');
+    $('#currentDay').text(currentDate);
+    
 });
+
